3804b6d175248dba4dcf6f5c1dc77a2871525454
Mansur <mansur@MacBook-Pro.local>
2026-08-22 22:12:52 +0300

    feat: инициализация проекта и базовый функционал тренажера

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MathTrainer from './components/MathTrainer'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <MathTrainer />
+    </div>
+  );
+}
+
+export default App;
